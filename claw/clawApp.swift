0359460ad5f3b4eaaa1276ecc2d5cacb457d9584
dhanuj <dhanujg499@gmail.com>
2026-02-28 18:13:08 -0500

    Initial Commit

---
+++ claw/clawApp.swift
@@ -0,0 +1,17 @@
+//
+//  clawApp.swift
+//  claw
+//
+//  Created by dhanu on 2/28/26.
+//
+
+import SwiftUI
+
+@main
+struct clawApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
